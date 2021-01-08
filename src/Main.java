8a5a93e214dbe8f6d6db4b6b37d44d087151a4fd
Rishat Karimov <richskiter@gmail.com>
2021-01-08 16:44:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int payment = 2000;
+        int balance = 500;
+        int bonus;
+
+        if (payment>1000) {
+            bonus = payment / 100;
+            balance = balance + payment + bonus;
+        }
+        else {
+            balance = balance + payment;
+        }
+        System.out.println(balance);
+    }
+}
